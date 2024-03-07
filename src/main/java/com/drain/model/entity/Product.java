f7ce81d061f92ff16e3b4764d370907cc09387c1
Giornokhro <giornojstar7@gmail.com>
2024-03-07 00:03:21 -0500

    feat::sparkles: add brand and product entities

--- src/main/java/com/drain/model/entity/Product.java
+++ src/main/java/com/drain/model/entity/Product.java
@@ -5,8 +5,7 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
-import javax.persistence.Entity;
-import javax.persistence.Table;
+import javax.persistence.*;
 
 @Getter
 @Setter
@@ -15,4 +14,28 @@ import javax.persistence.Table;
 @Entity
 @Table(name = "product")
 public class Product {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "ID", nullable = false, unique = true)
+    private Long id;
+    @Column(name = "Name")
+    private String name;
+    @Column(name = "Height")
+    private double height;
+    @Column(name = "Width")
+    private double width;
+    @Column(name = "Color")
+    private String color;
+    @Column(name = "PdtDescription")
+    private String pdtDescription;
+    @Column(name = "QtyInStock")
+    private int qtyInStock;
+    @Column(name = "BuyPrice")
+    private double buyPrice;
+    @Column(name = "Discount")
+    private double discount;
+    @ManyToMany()
+    private Brand brandID;
+    @Column(name = "ProductType")
+    private String productType;
 }
