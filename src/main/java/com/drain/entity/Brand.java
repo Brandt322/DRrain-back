7b9724648a69ea0856548012aaec26cb9b194caf
jejejean <u19212532@utp.edu.pe>
2024-03-06 22:27:12 -0500

    feat: :sparkles: add entities

---
+++ src/main/java/com/drain/entity/Brand.java
@@ -0,0 +1,16 @@
+package com.drain.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.Table;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "brand")
+public class Brand {
+}
---
+++ src/main/java/com/drain/entity/Customer.java
@@ -0,0 +1,19 @@
+package com.drain.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "customer")
+public class Customer {
+    
+}
