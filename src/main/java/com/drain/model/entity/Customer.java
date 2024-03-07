36216f4925a708b5db8f2b1eb482a7916c6cc8ab
jejejean <u19212532@utp.edu.pe>
2024-03-07 00:10:45 -0500

    feat: :sparkles: add variables to the customer and payment entities

--- src/main/java/com/drain/model/entity/Customer.java
+++ src/main/java/com/drain/model/entity/Customer.java
@@ -5,8 +5,8 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
-import javax.persistence.Entity;
-import javax.persistence.Table;
+import javax.persistence.*;
+import java.util.List;
 
 @Getter
 @Setter
@@ -15,5 +15,40 @@ import javax.persistence.Table;
 @Entity
 @Table(name = "customer")
 public class Customer {
-    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "ID")
+    private Long id;
+
+    @Column(name = "UserName")
+    private String userName;
+
+    @Column(name = "LastName")
+    private String lastName;
+
+    @Column(name = "FirstName")
+    private String firstName;
+
+    @Column(name = "Phone")
+    private String phone;
+
+    @Column(name = "Address1")
+    private String address1;
+
+    @Column(name = "Address2")
+    private String address2;
+
+    @Column(name = "Country")
+    private String country;
+
+    @Column(name = "City")
+    private String city;
+
+    @Column(name = "Province")
+    private String province;
+
+    @Column(name = "PostalCode")
+    private int postalCode;
+    @OneToMany(mappedBy = "customer")
+    private List<Payment> payments;
 }
